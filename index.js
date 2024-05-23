2a156437930cb0dac0f01bc4d46a1c24a2c292bf
presti <alanzenezi@gmail.com>
2024-05-23 15:35:25 -0300

    Counter

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log("Henlo!");
+var decBtn = document.getElementById("decBtn");
+var incBtn = document.getElementById("incBtn");
+var reset = document.getElementById("resetBtn");
+var counter = document.getElementById("counter");
+var count = 0;
+incBtn.onclick = function () {
+    count++;
+    counter.textContent = String(count);
+};
+decBtn.onclick = function () {
+    count--;
+    counter.textContent = String(count);
+};
+reset.onclick = function () {
+    count = 0;
+    counter.textContent = String(count);
+};
--- index.ts
+++ index.ts
@@ -1,1 +1,20 @@
-console.log("Henlo!");
+const decBtn = <HTMLButtonElement>document.getElementById("decBtn");
+const incBtn = <HTMLButtonElement>document.getElementById("incBtn");
+const reset = <HTMLButtonElement>document.getElementById("resetBtn");
+const counter = <HTMLLabelElement>document.getElementById("counter");
+let count = 0;
+
+incBtn.onclick = function () {
+  count++;
+  counter.textContent = String(count);
+};
+
+decBtn.onclick = function () {
+  count--;
+  counter.textContent = String(count);
+};
+
+reset.onclick = function () {
+  count = 0;
+  counter.textContent = String(count);
+};
